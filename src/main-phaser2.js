30ec3bc09b57f1cf6bf15e6e6d4a7457bede0668
Andras Csizmadia <andras@vpmedia.hu>
2023-09-16 18:32:10 +0200

    Use single quotes for js

--- src/main-phaser2.js
+++ src/main-phaser2.js
@@ -1,7 +1,7 @@
-import "./style.css";
-import { Game, Const } from "@vpmedia/phaser";
-import { generateBitmapFonts, loadImage } from "@vpmedia/bitcharify";
-import { addToCache } from "@vpmedia/bitcharify-phaser2";
+import './style.css';
+import { Game, Const } from '@vpmedia/phaser';
+import { generateBitmapFonts, loadImage } from '@vpmedia/bitcharify';
+import { addToCache } from '@vpmedia/bitcharify-phaser2';
 
 class GameState {
   /**
@@ -17,21 +17,21 @@ class GameState {
    * Create game objects.
    */
   create() {
-    const testText = "ABCDEFGHIJKLMNOPQRSTUVXYZ01234567890abcdefghijklmnoprstuxyz";
-    const textStyle = { font: "Arial", fontSize: 24, fill: "#FFFFFF" };
+    const testText = 'ABCDEFGHIJKLMNOPQRSTUVXYZ01234567890abcdefghijklmnoprstuxyz';
+    const textStyle = { font: 'Arial', fontSize: 24, fill: '#FFFFFF' };
     this.game.add.text(10, 400, testText, textStyle);
     //
-    const canvas = document.createElement("canvas");
-    canvas.style.position = "absolute";
-    canvas.style.display = "block";
+    const canvas = document.createElement('canvas');
+    canvas.style.position = 'absolute';
+    canvas.style.display = 'block';
     document.body.appendChild(canvas);
     //
-    const styles = [{ fontFamily: "Arial", fontSize: 24, fill: "#FFFFFF" }];
+    const styles = [{ fontFamily: 'Arial', fontSize: 24, fill: '#FFFFFF' }];
     const results = generateBitmapFonts(canvas, styles);
     results.forEach((result) => {
       loadImage(result.imageData).then((event) => {
-        addToCache(this.game, "Arial_24px", result.fontData, event.target);
-        this.game.add.bitmapText(10, 370, "Arial_24px", testText, 24);
+        addToCache(this.game, 'Arial_24px', result.fontData, event.target);
+        this.game.add.bitmapText(10, 370, 'Arial_24px', testText, 24);
         // canvas.width = 1;
         // canvas.height = 1;
       });
@@ -49,6 +49,6 @@ const config = {
 };
 
 const game = new Game(config);
-game.state.add("Game", GameState);
-game.state.start("Game");
+game.state.add('Game', GameState);
+game.state.start('Game');
 window.game = game;
--- src/main-phaser3.js
+++ src/main-phaser3.js
@@ -1,28 +1,28 @@
-import "./style.css";
-import { Game } from "phaser";
-import { generateBitmapFont, loadImage } from "@vpmedia/bitcharify";
-import { addToCache } from "@vpmedia/bitcharify-phaser3";
+import './style.css';
+import { Game } from 'phaser';
+import { generateBitmapFont, loadImage } from '@vpmedia/bitcharify';
+import { addToCache } from '@vpmedia/bitcharify-phaser3';
 
 /**
  * TBD.
  */
 function create() {
   const scope = this;
-  const testText = "ABCDEFGHIJKLMNOPQRSTUVXYZ01234567890abcdefghijklmnoprstuxyz";
-  const textStyle = { fontFamily: "Arial", fontSize: 24, fill: "#FFFFFF" };
+  const testText = 'ABCDEFGHIJKLMNOPQRSTUVXYZ01234567890abcdefghijklmnoprstuxyz';
+  const textStyle = { fontFamily: 'Arial', fontSize: 24, fill: '#FFFFFF' };
   scope.add.text(10, 400, testText, textStyle);
   //
-  const canvas = document.createElement("canvas");
-  canvas.style.position = "absolute";
-  canvas.style.display = "block";
+  const canvas = document.createElement('canvas');
+  canvas.style.position = 'absolute';
+  canvas.style.display = 'block';
   document.body.appendChild(canvas);
   //
-  const fontStyle = { fontFamily: "Arial", fontSize: 24, fill: "#FFFFFF" };
+  const fontStyle = { fontFamily: 'Arial', fontSize: 24, fill: '#FFFFFF' };
   const fontData = generateBitmapFont(canvas, fontStyle);
   //
   loadImage(canvas.toDataURL()).then((event) => {
-    addToCache(scope.game, "Arial_24px", fontData, event.target);
-    scope.add.bitmapText(10, 370, "Arial_24px", testText, 24);
+    addToCache(scope.game, 'Arial_24px', fontData, event.target);
+    scope.add.bitmapText(10, 370, 'Arial_24px', testText, 24);
     // canvas.width = 1;
     // canvas.height = 1;
   });
@@ -31,7 +31,7 @@ function create() {
 new Game({
   width: 1000,
   height: 600,
-  backgroundColor: "#333333",
+  backgroundColor: '#333333',
   banner: true,
   antialiasGL: false,
   transparent: false,
